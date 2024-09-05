4dbb1ed330200072f6b97d114ca363e2535b6d26
Sabrina Natasya Bilbina <sabrinatasya170306@gmail.com>
2024-09-05 14:32:50 +0700

    dekalrasi input

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Dapatkan referensi ke elemen input dengan ID "weight"
-let weight = document.getElementById("weight");
+const weight = document.getElementById("weight");
 
 // Dapatkan referensi ke elemen input dengan ID "height"
-let height = document.getElementById("height");
+const height = document.getElementById("height");
 
 // Dapatkan radio button yang dipilih dari grup "gender"
-let genderRadio = document.querySelector('input[name="gender"]:checked');
+const genderRadio = document.querySelector('input[name="gender"]:checked');
 
 // Dapatkan referensi ke elemen form dengan ID "form"
-let form = document.getElementById("form");
+const form = document.getElementById("form");
 
 // Dapatkan referensi ke elemen dengan class "comment" (area hasil)
-let resultArea = document.querySelector(".comment");
+const resultArea = document.querySelector(".comment");
 
 // Referensi elemen modal (opsional)
 modalContent = document.querySelector(".modal-content");
