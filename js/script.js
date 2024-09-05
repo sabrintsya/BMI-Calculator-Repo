03f2f02ec5eec3fdf5c3cac07eeb8ea7ba945f3b
Sabrina Natasya Bilbina <sabrinatasya170306@gmail.com>
2024-09-05 15:16:53 +0700

    menambahkan function bmi calculator dan modal close

--- js/script.js
+++ js/script.js
@@ -18,3 +18,52 @@ modalContent = document.querySelector(".modal-content");
 modalText = document.querySelector("#modalText");
 const modal = document.getElementById("myModal");
 const span = document.getElementsByClassName("close")[0];
+
+function calculate() {
+    // Periksa apakah semua input terisi
+    if (weight.value === "" || height.value === "" || !genderRadio) {
+        modal.style.display = "block";
+        modalText.innerHTML = `Semua kolom harus diisi!`;
+        return; // keluar dari fungsi jika ada input kosong
+    }
+
+    countBmi ();
+}
+
+function countBmi () {
+    // Konversi nilai input ke angka
+    let weightValue = Number(weight.value);
+    let heightValue = Number(height.value);
+
+    // Hitung BMI
+    let bmi = weightValue / (heightValue * heightValue);
+
+    // Tentukan kategori BMI
+    let result = '';
+    if (bmi < 18.5) {
+        result = 'Underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        result = 'Normal';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        result = 'Overweight';
+    } else if (bmi >= 35) {
+        result = 'Obesity';
+    }
+
+    // Tampilkan Hasil
+    resultArea.style.display ="block";
+    document.querySelector(".comment").innerHTML = `Berat badan Anda <span id="status">${result}</span>`;
+    document.querySelector("#result").innerHTML = bmi.toFixed(2);
+}
+
+// Tutup modal ketika tombol tutup (class "close") diklik
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Tutup modal ketika diklik diluar area modal 
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
